464c02aebbf43fed6a6ac12bb487c16daa21f3a9
CESAR GUERRA <cguerra@gmail.com>
2020-01-16 00:25:50 -0500

    ACTUALIZANDO

--- src/main/java/pe/com/capacitacion/MainApp.java
+++ src/main/java/pe/com/capacitacion/MainApp.java
@@ -14,74 +14,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @EnableAdminServer
  public class MainApp{
  
-		private String adminServer; 
-	/*
-		public MainApp( AdminServerProperties adminServer ){
-			   this.adminServer = adminServer.getContextPath();
-		}
- 
-		*/
 	    public static void main( String[] argumentos ){ 
 		 	   SpringApplication.run( MainApp.class, argumentos );
 	    }
- 
-	    /*
-		@Bean 
-		public SecurityWebFilterChain securityWebFilterChainSecure( ServerHttpSecurity http ){
-	
-			return http.authorizeExchange()
-					   .pathMatchers( this.adminServer + "/assets/**" )
-					   .permitAll()
-					   .pathMatchers( this.adminServer + "/login" )
-					   .permitAll()
-					   .anyExchange()
-					   .authenticated()
-					   .and()
-					   .formLogin()
-					   .loginPage( this.adminServer + "/login" )
-					   .and()
-					   .logout()
-					   .logoutUrl( this.adminServer + "/logout" )
-					   .and()
-					   .httpBasic()
-					   .and()
-					   .csrf()
-					   .disable()
-					   .build(); 
-		}
- */
-		
-		
-		
-		
-	    /*
-		@Bean
-		@Profile("insecure")
-		public SecurityWebFilterChain securityWebFilterChainPermitAll(ServerHttpSecurity http) {
-			return http.authorizeExchange((authorizeExchange) -> authorizeExchange.anyExchange().permitAll())
-					.csrf(ServerHttpSecurity.CsrfSpec::disable).build();
-		}
-		 */
-		
-        /*
-		@Bean
-		@Profile("secure")
-		public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
-			return http
-					.authorizeExchange((authorizeExchange) -> authorizeExchange
-							.pathMatchers(this.adminServer + "/assets/**" ).permitAll()
-							.pathMatchers(this.adminServer + "/login" ).permitAll().anyExchange().authenticated())
-					.formLogin((formLogin) -> formLogin.loginPage(this.adminServer + "/login" ))
-					.logout((logout) -> logout.logoutUrl(this.adminServer  + "/logout" ))
-					.httpBasic(Customizer.withDefaults()).csrf(ServerHttpSecurity.CsrfSpec::disable).build();
-		}
-		*/
-		
-        /*
-		@Bean
-		public LoggingNotifier loggerNotifier(InstanceRepository repository) {
-			return new LoggingNotifier(repository);
-		}
-        */
+  
  }
  
