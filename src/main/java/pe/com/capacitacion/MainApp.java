eac91087a02027d65b3516856e8fd046fd5769a6
CESAR GUERRA <cguerra@gmail.com>
2020-01-15 22:39:47 -0500

    ACTUALIZANDO

--- src/main/java/pe/com/capacitacion/MainApp.java
+++ src/main/java/pe/com/capacitacion/MainApp.java
@@ -1,47 +1,47 @@
 package pe.com.capacitacion;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration; 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
+ 
 /**
  * MainApp
  * @author cguerra
  **/
- @SpringBootApplication
- @EnableAdminServer        //IMPORTANTE: 'BOOT-ADMIN SERVER'
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
  public class MainApp{
-	
-		private final String objAdminContextPath; 
-	
-		public MainApp(AdminServerProperties adminServerProperties) {
-			   this.objAdminContextPath = adminServerProperties.getContextPath();
+ 
+		private String adminServer; 
+	/*
+		public MainApp( AdminServerProperties adminServer ){
+			   this.adminServer = adminServer.getContextPath();
 		}
-	
-		public static void main( String[] argumentos ){
-			   SpringApplication.run( MainApp.class, argumentos );
-		}
-	
-		@Bean
+ 
+		*/
+	    public static void main( String[] argumentos ){ 
+		 	   SpringApplication.run( MainApp.class, argumentos );
+	    }
+ 
+	    /*
+		@Bean 
 		public SecurityWebFilterChain securityWebFilterChainSecure( ServerHttpSecurity http ){
 	
 			return http.authorizeExchange()
-					   .pathMatchers( this.objAdminContextPath + "/assets/**" )
+					   .pathMatchers( this.adminServer + "/assets/**" )
 					   .permitAll()
-					   .pathMatchers( this.objAdminContextPath + "/login" )
+					   .pathMatchers( this.adminServer + "/login" )
 					   .permitAll()
 					   .anyExchange()
 					   .authenticated()
 					   .and()
 					   .formLogin()
-					   .loginPage( this.objAdminContextPath + "/login" )
+					   .loginPage( this.adminServer + "/login" )
 					   .and()
 					   .logout()
-					   .logoutUrl( this.objAdminContextPath + "/logout" )
+					   .logoutUrl( this.adminServer + "/logout" )
 					   .and()
 					   .httpBasic()
 					   .and()
@@ -49,6 +49,39 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 					   .disable()
 					   .build(); 
 		}
-
+ */
+		
+		
+		
+		
+	    /*
+		@Bean
+		@Profile("insecure")
+		public SecurityWebFilterChain securityWebFilterChainPermitAll(ServerHttpSecurity http) {
+			return http.authorizeExchange((authorizeExchange) -> authorizeExchange.anyExchange().permitAll())
+					.csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+		}
+		 */
+		
+        /*
+		@Bean
+		@Profile("secure")
+		public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
+			return http
+					.authorizeExchange((authorizeExchange) -> authorizeExchange
+							.pathMatchers(this.adminServer + "/assets/**" ).permitAll()
+							.pathMatchers(this.adminServer + "/login" ).permitAll().anyExchange().authenticated())
+					.formLogin((formLogin) -> formLogin.loginPage(this.adminServer + "/login" ))
+					.logout((logout) -> logout.logoutUrl(this.adminServer  + "/logout" ))
+					.httpBasic(Customizer.withDefaults()).csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+		}
+		*/
+		
+        /*
+		@Bean
+		public LoggingNotifier loggerNotifier(InstanceRepository repository) {
+			return new LoggingNotifier(repository);
+		}
+        */
  }
  
