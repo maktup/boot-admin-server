afcecd55f3b257e3bd2da82650b40adaea10c475
CESAR GUERRA <cguerra@gmail.com>
2020-01-16 01:18:15 -0500

    ACTUALIZANDO

--- src/main/java/pe/com/capacitacion/MainApp.java
+++ src/main/java/pe/com/capacitacion/MainApp.java
@@ -2,16 +2,26 @@ package pe.com.capacitacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
  
 /**
  * MainApp
  * @author cguerra
  **/
-@Configuration
-@EnableAutoConfiguration
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableAdminServer
+
+@SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
+@Configuration
  public class MainApp{
  
 	    public static void main( String[] argumentos ){ 
